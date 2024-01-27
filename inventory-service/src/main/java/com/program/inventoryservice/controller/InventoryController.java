c6cc0a34a89ba09089e427c34571a1a66be0d9f3
Duy Nguyen <duy.nguyen2412@protonmail.com>
2024-01-27 16:25:40 +0100

    set up database connection, successfully insert data to db

---
+++ inventory-service/src/main/java/com/program/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,18 @@
+package com.program.inventoryservice.controller;
+
+import com.program.inventoryservice.service.InventoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+public class InventoryController {
+
+    private InventoryService inventoryService;
+
+    @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+        return inventoryService.isInStock(skuCode);
+    }
+}
